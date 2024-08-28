a447bd5a019a83af05ba4f9c8d46844bf218a51d
wmontoya0103 <84595614+wmontoya0103@users.noreply.github.com>
2024-08-28 18:44:27 +0000

    Corrección de código para el reto 2

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,26 +1,33 @@
 // Placeholder to host a component
-import React from 'react';
+import React from "react";
 
-const Hours = () => {
-  // Obtener la fecha actual
-  const currentDate = new Date();
-  
-  // Crear un array con las horas del día (00:00 a 23:00)
-  const hours = Array.from({ length: 24 }, (_, i) => {
-    const hour = i < 10 ? `0${i}:00` : `${i}:00`;
-    return `${hour}`;
-  });
+    const Hours = () => {
+        // add logic
 
-  return (
-    <div>
-      <h2>Horas del día ({currentDate.toLocaleDateString()}):</h2>
-      <ul>
-        {hours.map((hour, index) => (
-          <li key={index}>{hour}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+        const shelterHours = [
+            { day: "Monday", open: "10:00", close: "16:00" },
+            { day: "Tuesday", open: "10:00", close: "16:00" },
+            { day: "Wednesday", open: "10:00", close: "16:00" },
+            { day: "Thursday", open: "10:00", close: "16:00" },
+            { day: "Friday", open: "10:00", close: "16:00" },
+            { day: "Saturday", open: "9:00", close: "20:00" },
+            { day: "Sunday", open: "9:00", close: "20:00" },
+    
+        ]
+
+        // get the long day name and store it in a variable called today
+        const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+
+        // get today's hours
+        const todayHours = shelterHours.find((day) => day.day === today);
+
+        // display today and the hours in an div with an id of hours
+        return (
+            <div id="hours">
+                <h2>Today's Hours</h2>
+                <p>{todayHours.day} {todayHours.open} - {todayHours.close}</p>
+            </div>
+        )
+    }
 
 export default Hours;
